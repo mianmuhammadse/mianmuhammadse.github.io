9653d28465bba3aa2945e6476db90faed8cf8472
Mian Muhammad <mianmuhammad660@gmail.com>
2025-01-11 15:21:40 +0500

    add deploy repo

--- astro.config.ts
+++ astro.config.ts
@@ -96,7 +96,8 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
+	site: "https://mianmuhammadse.github.io",
+  "base": "/mianmuhammadse",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
